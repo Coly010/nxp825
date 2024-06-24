2db2ac17cf1de4de2e33123919c876fcbdbe76e4
Colum Ferry <cferry09@gmail.com>
2024-06-24 15:53:57 +0100

    migrate cypress

--- apps/app1-e2e/cypress.config.ts
+++ apps/app1-e2e/cypress.config.ts
@@ -4,7 +4,16 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    ...nxE2EPreset(__filename, { cypressDir: 'src', bundler: 'vite' }),
+    ...nxE2EPreset(__filename, {
+      ciWebServerCommand: 'npx nx run app1:serve-static',
+      webServerCommands: {
+        default: 'npx nx run app1:serve',
+        production: 'npx nx run app1:serve:production',
+        ci: 'npx nx run app1:serve-static',
+      },
+      cypressDir: 'src',
+      bundler: 'vite',
+    }),
     baseUrl: 'http://localhost:4200',
   },
 });
